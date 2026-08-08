c786fdd86d502c068cce08673eae510b6d9cecb3
nodiappu <nodiappu@nodiappu-ThinkCentre-M700>
2026-08-08 05:32:03 +0530

    fix: updated conversion constants to include class 8 for 80/20 split

--- src/pages/SubjectMarks.jsx
+++ src/pages/SubjectMarks.jsx
@@ -12,7 +12,7 @@ import { useReactTable, getCoreRowModel, flexRender, getSortedRowModel, getFilte
 
 export const getConversionConstants = (className) => {
   if (!className) return { examConv: 75, testMax: 25 };
-  const isHigher = /(9|10|11|12|ix|x|xi|xii)\b/i.test(className);
+  const isHigher = /(8|9|10|11|12|viii|ix|x|xi|xii)\b/i.test(className);
   return isHigher ? { examConv: 80, testMax: 20 } : { examConv: 75, testMax: 25 };
 };
 
